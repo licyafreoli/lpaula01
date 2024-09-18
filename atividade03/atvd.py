2d7fbb4d1255de42a28478736ab257ebb0bee59c
Rossana Lícya <168486966+licyafreoli@users.noreply.github.com>
2024-09-18 00:47:16 -0300

    Add files via upload

---
+++ atividade03/atvd.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+cidade_natal = input("Digite sua cidade natal: ")
+
+print(f"Olá, {nome}! Você tem {idade} anos e é de {cidade_natal}.")
